52065edb582087584a6c376628dec3c1012f852f
ares-123-gpa <ft030725@gmail.com>
2026-02-01 07:19:39 +0200

    Add new features in button AddToCart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,10 +2,22 @@ import { useContext } from "react";
 import { IoCartOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import { useUser } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
 
 export default function ProductCard({product}) { 
     const {addToCart} = useContext(CartContext)  
+  
+    const {user} = useUser() 
+    const isLogin = (product)=>{
+      if(user){
+        addToCart(product)
+      }else{
+        toast.error("You need to log in first to add items to your cart")
+      }
+    }
+
   return <>
   <div className='relative border border-gray-100 rounded-2xl cursor-pointer hover:scale-105  hover:shadow-2xl transition-all p-2 h-max'>
     
@@ -13,7 +25,7 @@ export default function ProductCard({product}) {
       
     <h1 className="line-clamp-2 p-1 font-semibold">{product.title.length > 10 ? product.title.slice(0,10):product.title}</h1>
     <p className="font-bold my-1 text-lg text-gray-800">${product.price}</p>
-    <button onClick={()=>addToCart(product)} className="flex items-center gap-2 bg-red-500 text-white px-2 py-1 rounded-md font-semibold text-lg w-full cursor-pointer"><span><IoCartOutline className="w-6 h-6"/></span> Add to Cart</button>  
+    <button onClick={()=>isLogin(product)} className="flex items-center gap-2 bg-red-500 text-white px-2 py-1 rounded-md font-semibold text-lg w-full cursor-pointer"><span><IoCartOutline className="w-6 h-6"/></span> Add to Cart</button>  
   </div>
   
   </> 
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,17 +1,19 @@
 import axios from 'axios'
 import { useContext, useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Loading from "../assets/src_assets_Loading4.webm"
 import Breadcrums from '../components/Breadcrums'
 
 import { IoCartOutline } from 'react-icons/io5'
 import { CartContext } from '../context/CartContext'
+import { toast } from 'react-toastify'
+import { useUser } from '@clerk/clerk-react'
 
 export default function SingleProduct() {
     let { id } = useParams()
-    const navigate = useNavigate() 
-    const [productData, setProductData] = useState() 
-    const {addToCart} = useContext(CartContext)
+    const {user} = useUser() 
+    const [productData, setProductData] = useState()
+    const { addToCart } = useContext(CartContext)
 
     const fetchSingleProduct = async () => {
         try {
@@ -22,9 +24,18 @@ export default function SingleProduct() {
             console.log(error)
         }
     }
+
+    const isLogin = (product) => {
+        if (user) {
+            addToCart(product)
+        } else {
+            toast.error("You need to log in first to add items to your cart")
+        }
+    }
+
     useEffect(() => {
         fetchSingleProduct();
-        window.scroll(0,0)
+        window.scroll(0, 0)
     }, [])
     return <>
         {
@@ -32,8 +43,8 @@ export default function SingleProduct() {
                 <Breadcrums title={productData.title} />
                 <div className='max-w-6xl mx-auto md:p-6 grid md:grid-cols-2 gap-10'>
                     {/* Product Image */}
-                    <div className='w-full'> 
-                        <img src={productData.image} alt={productData.title} className=' object-cover'/>
+                    <div className='w-full'>
+                        <img src={productData.image} alt={productData.title} className=' object-cover' />
                     </div>
                     {/* Product Details */}
                     <div>
@@ -42,11 +53,11 @@ export default function SingleProduct() {
                         <p className='font-bold text-xl'>Price: <span className='text-red-500 font-semibold text-lg'>${productData.price}</span></p>
                         <p className='text-gray-500 py-5'>{productData.description}</p>
                         <label className='font-semibold'>Quantity: </label>
-                        <input type='number' className='w-15 placeholder:text-black pl-2' min={1} placeholder='1' value={1} readOnly/>
-                        <p className='font-semibold text-lg pt-4'>Rate: <span className='text-green-500'>{productData.rating.rate}</span></p> 
+                        <input type='number' className='w-15 placeholder:text-black pl-2' min={1} placeholder='1' value={1} readOnly />
+                        <p className='font-semibold text-lg pt-4'>Rate: <span className='text-green-500'>{productData.rating.rate}</span></p>
                         <p className='font-semibold text-lg pb-4'>Count: <span className='text-blue-500'>{productData.rating.count}</span></p>
                         <div className='flex gap-4 mt-4'>
-                                <button onClick={()=>addToCart(productData)} className='px-6 flex gap-2 py-2 text-lg bg-red-500 text-white rounded-md cursor-pointer'><IoCartOutline className='w-6 h-6'/> Add to Cart</button>
+                            <button onClick={() => isLogin(productData)} className='px-6 flex gap-2 py-2 text-lg bg-red-500 text-white rounded-md cursor-pointer'><IoCartOutline className='w-6 h-6' /> Add to Cart</button>
                         </div>
                     </div>
                 </div>
